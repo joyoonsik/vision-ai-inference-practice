9082e83e5275a72b8120bef6e7f84948be5d4902
joyoonsik <ysixjo@gmail.com>
2025-01-24 18:33:59 +0900

    1

--- 5. conveyor-system/conveyor-system-practice.py
+++ 5. conveyor-system/conveyor-system-practice.py
@@ -4,14 +4,21 @@ import requests
 import numpy
 from io import BytesIO
 from pprint import pprint
+import globals
+import os
 
 import cv2
 
 ser = serial.Serial("/dev/ttyACM0", 9600)
 
+
 # API endpoint
 api_url = ""
 
+dataset_dir = 'dataset'
+output_dir = 'dataset_crop'
+
+# crop_size
 
 def get_img():
     """Get Image From USB Camera
@@ -69,21 +76,52 @@ def inference_reqeust(img: numpy.array, api_rul: str):
     except requests.exceptions.RequestException as e:
         print(f"Error sending request: {e}")
 
+save_folder = 'dataset_crop'
+
+save_folder = 'dataset'
+if not os.path.exists(save_folder):
+    os.makedirs(save_folder)
+
+camera = cv2.VideoCapture(0)
+import time
+
 
 while 1:
     data = ser.read()
-    print(data)
+
     if data == b"0":
-        img = get_img()
-        # crop_info = None
-        crop_info = {"x": 200, "y": 100, "width": 300, "height": 300}
-
-        if crop_info is not None:
-            img = crop_img(img, crop_info)
-
-        cv2.imshow("", img)
-        cv2.waitKey(1)
-        result = inference_reqeust(img, api_url)
+        time.sleep(2)
+        f, img = camera.read()    
+        timestamp = time.strftime("%Y%m%d_%H%M%S")  # 현재 시간을 기반으로 파일명 생성
+        img_filename = os.path.join(save_folder, f"image_{timestamp}.jpg")
+        cv2.imwrite(img_filename, img)
+        time.sleep(1)
+        
         ser.write(b"1")
+        
+        
+    #     print("ice!!!")
+        # img = get_img()
+        # # crop_info = None
+        # crop_info = {"x": 200, "y": 100, "width": 300, "height": 100}
+
+        # if crop_info is not None:
+        #     img = crop_img(img, crop_info)
+
+        # timestamp = time.strftime("%Y%m%d_%H%M%S")
+        # #save_folder = 'dataset_crop'
+        # img_filename = os.path.join(save_folder, f"image_{timestamp}.jpg")
+        # print(img_filename)
+        # cv2.imwrite(img_filename, img)
+
+        # cv2.imshow("", img)
+        # cv2.waitKey(1)
+        # result = inference_reqeust(img, api_url)
+        # ser.write(b"1")
     else:
         pass
+    
+    
+
+
+
--- 5. conveyor-system/video_stream.py
+++ 5. conveyor-system/video_stream.py
@@ -1,13 +1,43 @@
 import cv2
+import time
+import os
+import globals
+
+save_folder = 'dataset'
+if not os.path.exists(save_folder):
+    os.makedirs(save_folder)
 
 camera = cv2.VideoCapture(0)
 video = cv2.VideoWriter("video.avi", -1, 25, (640, 480))
 while True:
     f, img = camera.read()
-
+    print(globals.a)
+    if globals.a:
+        timestamp = time.strftime("%Y%m%d_%H%M%S")  # 현재 시간을 기반으로 파일명 생성
+        img_filename = os.path.join(save_folder, f"image_{timestamp}.jpg")
+        print(img_filename)
+        cv2.imwrite(img_filename, img)
+        globals.a = False
+    
+    
     video.write(img)
 
     cv2.imshow("webcam", img)
     if cv2.waitKey(5) != -1:
         break
 video.release()
+
+
+# import cv2
+
+# camera = cv2.VideoCapture(0)
+# video = cv2.VideoWriter("video.avi", -1, 25, (640, 480))
+# while True:
+#     f, img = camera.read()
+
+#     video.write(img)
+
+#     cv2.imshow("webcam", img)
+#     if cv2.waitKey(5) != -1:
+#         break
+# video.release()
